734c71845ff84cbb2033a2f195cfc7251570b7c5
tomaszblock <127046228+tomaszblock@users.noreply.github.com>
2024-02-17 19:35:09 +0100

    done

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 import { fetchBreeds } from './cat-api';
 import { fetchCatByBreed } from './cat-api';
-import axios from 'axios';
 
 const breedSelect = document.querySelector('.breed-select');
 const catInfo = document.querySelector('.cat-info');
 const loader = document.querySelector('.loader');
+const errorEle = document.querySelector('.error');
 
 try {
   loader.classList.remove('hidden');
+  errorEle.style.display = 'none';
   fetchBreeds().then(data => renderSelect(data));
 } catch (error) {
+  errorEle.style.display = 'block';
   console.log(error);
 }
 
@@ -21,24 +23,29 @@ function renderSelect(breeds) {
     .join('');
   breedSelect.insertAdjacentHTML('beforeend', markup);
   loader.classList.add('hidden');
+  errorEle.style.display = 'none';
 }
 
 breedSelect.addEventListener('change', e => {
   loader.classList.remove('hidden');
+  errorEle.style.display = 'none';
   fetchCatByBreed(e.target.value).then(data => renderCat(data[0]));
 });
 
 function renderCat(catData) {
   const { url } = catData;
   const { description, name, temperament } = catData.breeds[0];
-
+  const catResult = document.querySelector('.cats');
+  if (catResult !== null) {
+    catResult.remove();
+  }
   catInfo.insertAdjacentHTML(
     'beforeend',
-    `<div class="dupa">
+    `<div class="cats">
     <h2>${name}</h2>
-    <img src="${url}" alt="${name}" />
     <p>${description}</p>
     <p><strong>Temperament:</strong> ${temperament}</p>
+    <img src="${url}" alt="${name}" />
     </div>`
   );
   loader.classList.add('hidden');
