061b1c97be8ea51955d08748951ff7922ac1e1c7
tomaszblock <127046228+tomaszblock@users.noreply.github.com>
2024-02-21 10:19:26 +0100

    errors added

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import { fetchBreeds } from './cat-api';
 import { fetchCatByBreed } from './cat-api';
+import Notiflix from 'notiflix';
 
 const breedSelect = document.querySelector('.breed-select');
 const catInfo = document.querySelector('.cat-info');
@@ -12,7 +13,9 @@ try {
   fetchBreeds().then(data => renderSelect(data));
 } catch (error) {
   errorEle.style.display = 'block';
-  console.log(error);
+  Notiflix.Notify.failure(
+    'Oops! Something went wrong! Try reloading the page!'
+  );
 }
 
 function renderSelect(breeds) {
@@ -33,20 +36,26 @@ breedSelect.addEventListener('change', e => {
 });
 
 function renderCat(catData) {
-  const { url } = catData;
-  const { description, name, temperament } = catData.breeds[0];
-  const catResult = document.querySelector('.cats');
-  if (catResult !== null) {
-    catResult.remove();
-  }
-  catInfo.insertAdjacentHTML(
-    'beforeend',
-    `<div class="cats">
+  if (catData != undefined) {
+    const { url } = catData;
+    const { description, name, temperament } = catData.breeds[0];
+    const catResult = document.querySelector('.cats');
+    if (catResult !== null) {
+      catResult.remove();
+    }
+    catInfo.insertAdjacentHTML(
+      'beforeend',
+      `<div class="cats">
     <h2>${name}</h2>
     <p>${description}</p>
     <p><strong>Temperament:</strong> ${temperament}</p>
     <img src="${url}" alt="${name}" />
     </div>`
-  );
+    );
+  } else {
+    Notiflix.Notify.failure(
+      'Oops! Something went wrong! Try reloading the page!'
+    );
+  }
   loader.classList.add('hidden');
 }
